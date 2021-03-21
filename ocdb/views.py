7fc39877ecd414a301f17832d7ed255e48ef6f17
Wolfram Keil <wolframkeil@gmx.net>
2021-03-21 18:10:41 +0100

    (FEAT) add filter option to get ascents for diary

--- ocdb/views.py
+++ ocdb/views.py
@@ -4,6 +4,7 @@ from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from ocdb import serializers
 from ocdb import models
@@ -158,6 +159,10 @@ class AscentViewSet(viewsets.ModelViewSet):
 
     queryset = models.Ascent.objects.all()
     serializer_class = serializers.AscentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = [
+        "fk_diary",
+    ]
 
 
 class RopePartyViewSet(viewsets.ModelViewSet):
