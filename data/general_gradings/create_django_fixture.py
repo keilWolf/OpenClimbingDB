fdc6a373aa7ae1d7e1a7960906ed695010447cef
Wolfram Keil <wkeildev@gmail.com>
2022-03-20 10:31:49 +0100

    feat: update base data
    - add saxony jumping
    - add technical climbing styles
    - update csv 2 fixture script
    - update fixtures

--- data/general_gradings/create_django_fixture.py
+++ data/general_gradings/create_django_fixture.py
@@ -1,98 +1,121 @@
+from dataclasses import dataclass
 import json
 import os
 from pathlib import Path
-import pandas
+from typing import List
 
 
 FIXTURES_OUT = "../../ocdb/fixtures/"
 
 
-class GradeSystemTypeCollection:
-    def __init__(self):
-        self.grade_system_types = []
-        self.count = 0
+@dataclass
+class Grade:
+    name: str
+    weight: int
 
-    def add(self, name):
-        self.count += 1
-        self.grade_system_types.append(
-            {
-                "pk": self.count,
-                "model": "ocdb.GradeSystemType",
-                "fields": {"name": name},
-            }
-        )
-
-    def export(self):
-        with open(f"{FIXTURES_OUT}grade_system_type_fixture.json", "w") as f:
-            json.dump(self.grade_system_types, f, indent=4)
+    def as_django_fixture(self, pk: int, fk: int):
+        return {
+            "pk": pk,
+            "model": "ocdb.Grade",
+            "fields": {
+                "name": self.name,
+                "weight": self.weight,
+                "fk_grade_system": fk,
+            },
+        }
 
 
-class GradeSystemCollection:
-    def __init__(self):
-        self.grade_system = []
-        self.count = 0
+@dataclass
+class GradeSystem:
+    name: str
+    grades: List[Grade]
 
-    def add(self, name, fk_grade_system_type):
-        self.count += 1
-        self.grade_system.append(
-            {
-                "pk": self.count,
-                "model": "ocdb.GradeSystem",
-                "fields": {"name": name, "fk_grade_system_type": fk_grade_system_type},
-            }
-        )
-
-    def export(self):
-        with open(f"{FIXTURES_OUT}grade_system_fixture.json", "w") as f:
-            json.dump(self.grade_system, f, indent=4)
+    def as_django_fixture(self, pk: int, fk: int):
+        return {
+            "pk": pk,
+            "model": "ocdb.GradeSystem",
+            "fields": {"name": self.name, "fk_grade_system_type": fk},
+        }
 
 
-class GradeCollection:
-    def __init__(self):
-        self.grades = []
-        self.count = 0
+@dataclass
+class GradeSystemType:
+    name: str
+    grade_systems: List[GradeSystem]
 
-    def add(self, name, weight, fk_grade_system):
-        self.count += 1
-        self.grades.append(
-            {
-                "pk": self.count,
-                "model": "ocdb.Grade",
-                "fields": {
-                    "name": name,
-                    "weight": weight,
-                    "fk_grade_system": fk_grade_system,
-                },
-            }
-        )
+    def as_django_fixture(self, pk: int):
+        return {
+            "pk": pk,
+            "model": "ocdb.GradeSystemType",
+            "fields": {"name": self.name},
+        }
 
-    def export(self):
-        with open(f"{FIXTURES_OUT}grade_fixture.json", "w") as f:
-            json.dump(self.grades, f, indent=4)
+
+def export(grade_system_types: List[GradeSystemType]):
+
+    types = []
+    systems = []
+    grades = []
+
+    pk_type = 0
+    pk_system = 0
+    pk_grade = 0
+
+    for gst in grade_system_types:
+        pk_type += 1
+        types.append(gst.as_django_fixture(pk_type))
+
+        for system in gst.grade_systems:
+            pk_system += 1
+            systems.append(system.as_django_fixture(pk_system, pk_type))
+
+            for grade in system.grades:
+                pk_grade += 1
+                grades.append(grade.as_django_fixture(pk_grade, pk_system))
+
+    with open(f"{FIXTURES_OUT}grade_system_type_fixture.json", "w") as f:
+        json.dump(types, f, indent=4)
+
+    with open(f"{FIXTURES_OUT}grade_system_fixture.json", "w") as f:
+        json.dump(systems, f, indent=4)
+
+    with open(f"{FIXTURES_OUT}grade_fixture.json", "w") as f:
+        json.dump(grades, f, indent=4)
 
 
 def main():
     path = Path(__file__)
     files = os.listdir(path.parent)
 
-    grade_system_types = GradeSystemTypeCollection()
-    grade_systems = GradeSystemCollection()
-    grade_collection = GradeCollection()
+    grade_system_types = []
 
     for file in files:
         if file.endswith(".csv"):
-            grade_system_types.add(Path(file).stem.capitalize())
-            data = pandas.read_csv(file)
-            for col_name in data.columns[1:]:
-                col = data[col_name]
-                grade_systems.add(col_name, grade_system_types.count)
-                for weight, row in enumerate(col):
-                    if pandas.notna(row):
-                        grade_collection.add(row, weight, grade_systems.count)
+            grade_system_type_name = Path(file).stem.capitalize()
+            grade_system_type = GradeSystemType(grade_system_type_name, [])
+            grade_system_types.append(grade_system_type)
 
-    grade_system_types.export()
-    grade_systems.export()
-    grade_collection.export()
+            with open(file, "r") as data:
+                # read header -> grade system names
+                grade_system_names = data.readline().split(",")[1:]
+                for gsn in grade_system_names:
+                    grade_system_name = gsn.replace("\n", "")
+                    grade_system = GradeSystem(grade_system_name, [])
+                    grade_system_type.grade_systems.append(grade_system)
+
+                # read rows
+                line = data.readline()
+                while line:
+                    row = line.split(",")
+                    weight = int(row[0])
+                    for i, grade in enumerate(row[1:]):
+                        grade = grade.replace("\n", "")
+                        if not grade == "":
+                            grade = Grade(grade, weight)
+                            grade_system_type.grade_systems[i].grades.append(grade)
+                    line = data.readline()
+
+    export(grade_system_types)
 
 
 if __name__ == "__main__":
